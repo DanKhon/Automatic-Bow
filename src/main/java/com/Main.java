53cafcd394cc8f3aa1c6ce00cfb9c6eb5208e03b
danikhan12 <65040851+danikhan12@users.noreply.github.com>
2020-09-13 09:02:45 -0700

    Update Main.java

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -9,6 +9,8 @@ public class Main extends PluginBase {
     public void onEnable() {
 
         getServer().getPluginManager().registerEvents(new Interact(),this);
+        getServer().getCommandMap().register("setautomatic",new Toggle("setautomatic","Toggles the automatic bow on and off","/setautomatic"));
+
         super.onEnable();
     }
     @Override
